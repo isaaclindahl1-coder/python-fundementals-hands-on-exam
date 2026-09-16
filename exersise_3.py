70ce908b8ff4c748a81d3a1e8d1bd3f601721c2a
Isaac Lindahl <cmp_islindahl@ects.org>
2026-09-16 09:54:27 -0400

    exersise3

---
+++ exersise_3.py
@@ -0,0 +1,8 @@
+message = input("Enter a message: ").strip()
+print(len(message))
+print(message[0])
+print(message[-1])
+print(message[0:3])
+print(message[-3:])
+print(message[0::2])
+print(message[::-1])
